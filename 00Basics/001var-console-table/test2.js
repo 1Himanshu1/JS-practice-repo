9e5db48e40d7c9c33163eb2c8d16f66171571cca
1Himanshu1 <87665368+1Himanshu1@users.noreply.github.com>
2024-02-26 18:18:36 +0000

    update

---
+++ 00Basics/001var-console-table/test2.js
@@ -0,0 +1,9 @@
+const id = 10;
+let names = 'Himanshu';
+var age = 26; // var should be avoided as its globally scoped and can be mistakenly changed
+car = "swift"; // this is a BAD practice
+console.log(names);
+
+// id = 2;
+names = "Priyanshu"; // var and let variables can be re-initialized but const variables cannot
+console.table([names,age,car]);
