e948001f3e26ea43e57d3396cef83fd58807f181
1Himanshu1 <87665368+1Himanshu1@users.noreply.github.com>
2024-03-03 06:09:22 +0000

    nums

--- 00Basics/006numsAndMaths.js
+++ 00Basics/006numsAndMaths.js
@@ -1,1 +1,16 @@
-const count = 10;
+const count = 10.88; // automatic defined number
+console.log(count); 
+
+const balance = new Number(10000); // specifically cast that it is a number by using new(object hence it will have key-value methods by prototype)
+console.log(balance);
+
+console.log(balance.toString().length); // converts into string
+console.log(balance.toFixed(2)); // decimal points
+
+console.log(count.toPrecision(2)); // round of to nearest whole num
+
+const num = 1112.34;
+console.log(num.toPrecision(3));
+
+const n = 10000000
+console.log(n.toLocaleString('en-IN'));
