9e8073640ed96d9419c42bf777db76cade7e3482
1Himanshu1 <87665368+1Himanshu1@users.noreply.github.com>
2024-03-04 19:23:42 +0000

    arrayUpdate

--- 01Basics/011Array.js
+++ 01Basics/011Array.js
@@ -1,39 +1,69 @@
-// array
+// Array
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+const myArr = [0, 1, 2, 3, 4, 5] // standard way to initialize array
+console.log(myArr);
+/* => The Array OBJECT, stores a collection of multiplt=e items under a single var name, and has members for performing common array operations
+ 1. JS arrays are re-sizable and can contain a mix of different data types
+ 2.  JS arrays are not associative arrays, i.e only integers value can be treated as indexes to access the elements of array
+ 3.  JS array are 0-indexed
+ 4.  JS array-COPY operations create SHALLOW-COPIES.(all std built-in Copy ops w/ JS OBJECTS create shallow copies, rather than DEEP-COPIES) 
+  SHALLOW COPY => Original and Copy both Point to same reference (any change in either will affect both of them)
+  DEEP COPY => Original and Copy do not point to same reference(treated as two different values)
+ 5. Each JS array has  length and Prototype{Array(0)} properties associated with it
+*/
 
-const myArr2 = new Array(1, 2, 3, 4)
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4) //(Object)
 // console.log(myArr[1]);
+// console.log(myArr2.length);
+// console.log(typeof(myArr)); // object
+// console.log(typeof(myArr2)); // object
 
-// Array methods
-
+// +++++++++++ Array methods ++++++++++++++
+// ****** 1 . push()  adds a element to the last index
 // myArr.push(6)
 // myArr.push(7)
+// console.log(myArr);
+
+// // 2. pop() removes last element from array
 // myArr.pop()
+// console.log(myArr);
 
+// // 3. unshift() shifts all elements of array to right and adds an ele in the 0 index
+// // very time consuming in large db
 // myArr.unshift(9)
-// myArr.shift()
+// console.log(myArr);
 
+// 4. shift() removes 1 ele from the array(0-indexed ele gets deleted)
+// myArr.shift()
+// console.log(myArr);
+
+// // 5. includes() - questionnaire methods=> gives true or false(boolean)
 // console.log(myArr.includes(9));
+
+//6. indexOf(ele) - gives -1 if ele is not in the array
+// myArr.shift()
 // console.log(myArr.indexOf(3));
 
+// 7. join() - converts all ele of array into string format
 // const newArr = myArr.join()
+// const newArr = myArr.join("-")
 
 // console.log(myArr);
 // console.log( newArr);
 
 
-// slice, splice
+// 8. slice(DO NOT Mutate original array), splice(Mutates original array)
 
 console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
-
 console.log(myn1);
-console.log("B ", myArr);
 
+console.log("B ", myArr); // original array is still intact after using slice
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
+
+console.log("C ", myArr); // original array has changed, i.e the splice range was deleted from the array
 console.log(myn2);
---
+++ 01Basics/012array.js
@@ -0,0 +1,42 @@
+// 1. CONCAT
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// BAD WAY TO CONCAT TWO ARRAYS
+
+// marvel_heros.push(dc_heros) // SLICE PUSHES INTO THE MAIN existing ARRAY
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// GOOD WAY TO CONCAT TWO ARRAYS
+// const allHeros = marvel_heros.concat(dc_heros) //returns new array,i.e CONCAT joins ele from two arrays and PUT THEM INTO A NEW ARRAY(which we have to catch in a variable)
+// console.log(allHeros);
+
+
+// 2. SPREAD OPERATOR
+
+// PROFESSIONAL WAY ( use spread operator[...])
+// const all_new_heros = [...marvel_heros, ...dc_heros] // kanch ka glass tut gya and all the ele inside it Bikhar gye by using [...arr_name]
+//        var       = [ first arr ke bikhare hue ele + second arr ke bhikhare ele ]
+// console.log(all_new_heros);
+
+
+// 3. flat(depth)
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// console.log(another_array);
+// const real_another_array = another_array.flat(Infinity) // flat(depth-lvl) just gives all nested arr elements into 1-single arrays
+// console.log(real_another_array);
+
+//4. Array methods
+// [.isArray,.from,.of]
+
+// console.log(Array.isArray("Hitesh")) // checks if given ele is array? =. returns boolean
+// console.log(Array.from("Hitesh")) // makes an array from given element
+// console.log(Array.from({name: "hitesh"})) // interesting(if it cant convert into array it returns EMPTY ARRAY = [])
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // makes an array from  given elements
