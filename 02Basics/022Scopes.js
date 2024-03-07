8ebe152084c092001794a4b997ff65281afe776a
1Himanshu1 <87665368+1Himanshu1@users.noreply.github.com>
2024-03-07 18:52:19 +0000

    scopes&HoistingBasics

--- 02Basics/022Scopes.js
+++ 02Basics/022Scopes.js
@@ -20,46 +20,46 @@
 // #
 /* in browser inspect's GLOBAL object  and your local global-var code run on node environment is DIFFERENT*/
 
-// #
-function one(){
-    const username = "hitesh"
+// # 2nd Part
+// function one(){
+//     const username = "himanshu"
 
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     two()
+//     console.log(website);
+// }
 
-     two()
+// one()
 
-}
 
-// // one()
-
+//#
 // if (true) {
 //     const username = "hitesh"
 //     if (username === "hitesh") {
 //         const website = " youtube"
-//         // console.log(username + website);
+//         console.log(username + website);
 //     }
 //     // console.log(website);
 // }
 
-// // console.log(username);
+// console.log(username);
 
 
-// // ++++++++++++++++++ interesting ++++++++++++++++++
+// ++++++++++++++++++ interesting ++++++++++++++++++
 
 
-// console.log(addone(5))
+console.log(addone(5))
 
-// function addone(num){
-//     return num + 1
-// }
+function addone(num){ // simple function
+    return num + 1
+}
 
 
-
-// addTwo(5)
-// const addTwo = function(num){
-//     return num + 2
-// }
+// #
+addTwo(5); // HOISTING: throws an error, becuz we've also used a (const addtwo(5))var to store the function inside it  
+let addTwo = function(num){ // also called function expression(funct stored in variables)
+    return num + 2
+}
