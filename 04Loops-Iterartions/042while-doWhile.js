09509ed9a7e42cd39b9405aa72f661679ae3948c
1Himanshu1 <87665368+1Himanshu1@users.noreply.github.com>
2024-03-11 14:28:54 +0000

    while

---
+++ 04Loops-Iterartions/042while-doWhile.js
@@ -0,0 +1,22 @@
+// while
+// #
+// let i = 1;  // intialization done separately here
+// while (i<=10) {  // only condition is checked inside - while(condition)
+//     console.log(`Value: ${i}`);
+//     i++; // exit condition is must for terminating the loop
+// }
+
+// #
+// let array = ["Flash", "Batman", "Superman"]
+// let i = 0
+// while (i<array.length) {
+//     console.log(` Hero is : ${array[i]}`);
+//     i++;
+// }
+
+// do-while (Atleast one time the code runs i.e one time logs the value then checks condition)
+let score = 11;
+do {
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score<=10); 
---
+++ 04Loops-Iterartions/043arrayLoops.js
@@ -0,0 +1,2 @@
+// Kuch loops specialized hote hai for arrays
+// as most data from DN and API get returned in form of arrays
